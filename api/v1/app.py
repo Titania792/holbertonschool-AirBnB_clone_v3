cff3ee6992300f2c6c840034ac64025091724f3e
Vinill <danimuller758@gmail.com>
2022-09-09 16:16:13 +0000

    fix all errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,22 +7,23 @@ from models import storage
 from flask import Flask
 import os
 
-app = Flask(__name____)
-app.register_blueprint(app_views
-
 
 host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
 port = os.environ.get('HBNB_API_PORT', '500')
+app = Flask(__name____)
+app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
-def close()
-"""
-Close session
-"""
+def close():
+    """
+    Close session
+    """
     storage.close()
 
+
 if __name__ == "__main__":
-"""
-Start api
-"""
-    app.run(host=host, port=port, t)
+    """
+    Start api
+    """
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ Init methods
 from api.v1.views import app_views
 from models import storage
 
+
 @app_views.route('/status')
 def status():
     """
